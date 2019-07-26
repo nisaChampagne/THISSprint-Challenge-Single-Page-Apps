dca9b9d3caf4d6e9b06b328a835787934c670ece
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-26 12:29:43 -0400

    semantic ui done

--- src/components/Character/CharacterCard.js
+++ src/components/Character/CharacterCard.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { Card } from 'semantic-ui-react'
+
 
 export default function CharacterCard (props) {
 
   return (
-  <span className='character-card'>
-    <img src={props.img} alt='rick and morty character'/>
-    <h2 className='character-name'>{props.name}</h2>
-    <p>Status: {props.status}</p>
-    <p>Species: {props.species}</p>
-    <p>Gender: {props.gender}</p>
-    <p>ID: {props.number}</p>
-  </span>
+
+    <Card
+    image={props.img} alt='rick and morty characters'
+    header={props.name}
+    meta={props.status}
+    description={props.species} 
+    extra={props.gender}/>
+  // <span className='character-card'>
+  //   <img src={props.img} alt='rick and morty character'/>
+  //   <h2 className='character-name'>{props.name}</h2>
+  //   <p>Status: {props.status}</p>
+  //   <p>Species: {props.species}</p>
+  //   <p>Gender: {props.gender}</p>
+  //   <p>ID: {props.number}</p>
+  // </span>
   );
 }
 
--- src/components/Locations/LocationCard.js
+++ src/components/Locations/LocationCard.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Card } from 'semantic-ui-react'
+
 
 export default function LocationCard (props) {
 
 
   return (
-    <span className='location-card'>
-      <h3>{props.name}</h3>
-      <h4>Type: {props.type}</h4>
-      <p>Dimension: {props.dimension}</p>
-    </span>
+
+    <Card
+    header={props.name}
+    meta={props.type}
+    description={props.dimension} />
+    // <span className='location-card'>
+    //   <h3>{props.name}</h3>
+    //   <h4>Type: {props.type}</h4>
+    //   <p>Dimension: {props.dimension}</p>
+    // </span>
   );
 }
 
