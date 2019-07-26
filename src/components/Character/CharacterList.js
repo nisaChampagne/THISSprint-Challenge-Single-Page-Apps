168bac2b37a30331bcd977ec223a766767476ce2
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-26 11:37:26 -0400

    readme, characters and episodes done

---
+++ src/components/Character/CharacterList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+
+export default function CharacterList(props) {
+    const [characters, setCharacters ] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/`)
+            .then(res => {
+                console.log('GUUD', res.data.results);
+                setCharacters(res.data.results)
+            })
+            .catch(error => {
+                console.error('OH NOES', error)
+            })
+    },[]);
+  return (
+    <section className='character-list grid-view'>
+     {characters.map(character => (
+              <CharacterCard 
+              img={character.image}
+              species={character.species}
+              name={character.name}
+              status={character.status}
+              number={character.id} />
+            ))}
+    </section>
+  );
+}
